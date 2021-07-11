10d7888590241c270f8ada78b4066f95593ad2ad
Phan Tấn Tài <phantantai9800@gmail.com>
2021-07-11 18:43:08 +0700

    tai - msbuild

--- eng/build.ps1
+++ eng/build.ps1
@@ -39,3 +39,59 @@ $artifactsdir = Join-Path $reporoot "artifacts"
 $logdir = Join-Path $artifactsdir "log"
 $logdir = Join-Path $logdir Windows_NT.$architecture.$configuration
 
+if ($ci) {
+    $remainingargs = "-ci " + $remainingargs
+}
+
+if ($bundletools) {
+    $remainingargs = "/p:BundleTools=true " + $remainingargs
+    $remainingargs = '/bl:"$logdir\BundleTools.binlog" ' + $remainingargs
+    $remainingargs = '-noBl ' + $remainingargs
+    $skipnative = $True
+    $test = $False
+}
+
+# Remove the private build registry keys
+if ($cleanupprivatebuild) {
+    Invoke-Expression "& `"$engroot\common\msbuild.ps1`" $engroot\CleanupPrivateBuild.csproj /v:$verbosity /t:CleanupPrivateBuild /p:BuildArch=$architecture /p:TestArchitectures=$architecture"
+    exit $lastExitCode
+}
+
+# Install sdk for building, restore and build managed components.
+if (-not $skipmanaged) {
+    Invoke-Expression "& `"$engroot\common\build.ps1`" -build -configuration $configuration -verbosity $verbosity /p:BuildArch=$architecture /p:TestArchitectures=$architecture $remainingargs"
+    if ($lastExitCode -ne 0) {
+        exit $lastExitCode
+    }
+}
+
+# Build native components
+if (-not $skipnative) {
+    Invoke-Expression "& `"$engroot\Build-Native.cmd`" -architecture $architecture -configuration $configuration -verbosity $verbosity $remainingargs"
+    if ($lastExitCode -ne 0) {
+        exit $lastExitCode
+    }
+}
+
+# Run the xunit tests
+if ($test) {
+    if (-not $crossbuild) {
+        & "$engroot\common\build.ps1" `
+          -test `
+          -configuration $configuration `
+          -verbosity $verbosity `
+          -ci:$ci `
+          /bl:$logdir\Test.binlog `
+          /p:BuildArch=$architecture `
+          /p:TestArchitectures=$architecture `
+          /p:PrivateBuildPath="$privatebuildpath" `
+          /p:DotnetRuntimeVersion="$dotnetruntimeversion" `
+          /p:DotnetRuntimeDownloadVersion="$dotnetruntimedownloadversion" `
+          /p:RuntimeSourceFeed="$runtimesourcefeed" `
+          /p:RuntimeSourceFeedKey="$runtimesourcefeedkey"
+
+        if ($lastExitCode -ne 0) {
+            exit $lastExitCode
+        }
+    }
+}
